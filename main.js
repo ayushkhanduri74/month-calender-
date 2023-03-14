b54533b0922afd26abffe74499b02bffd7689d4e
sahil sharma <113684157+ayushkhanduri78@users.noreply.github.com>
2023-03-14 20:29:30 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+ const monthEl = document.querySelector(".date h1");
+ const fullDateEl = document.querySelector(".date p");
+ const daysEl = document.querySelector(".days");
+
+
+
+// import monthes data using js
+
+const monthInx = new Date().getMonth();
+ const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate(); 
+const firstDay = new Date(new Date().getFullYear(), monthInx,1).getDay() - 1;
+
+
+const months = [
+    "January",
+    "Fabruary",
+    "March",
+    "April",
+    "May",
+    "Jun",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+monthEl.innerText = months[monthInx];
+fullDateEl.innerHTML = new Date().toDateString();
+
+let days = "";
+
+for(let i = firstDay; i>0; i--){
+    days += `<div class= "empty"></div>`;
+}
+for(let i =1; i<= lastDay; i++){
+    if(i === new Date().getDate()){
+        days += `<div class="today">${i}</div>`;
+    }else{
+        days += `<div>${i}</div>`
+    }
+ }
+
+daysEl.innerHTML = days;
+
+
+
